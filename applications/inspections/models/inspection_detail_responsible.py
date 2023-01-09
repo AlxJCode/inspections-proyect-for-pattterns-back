887b0790c8d88f22a3dc66b5fde0caffcfabe1ee
alex <alexarroyop31@gmail.com>
2023-01-09 10:19:58 -0500

    changes names models

--- applications/inspections/models/inspection_detail_responsible.py
+++ applications/inspections/models/inspection_detail_responsible.py
@@ -5,12 +5,12 @@ from applications.inspections.models import InspectionDetail
 from applications.users.models import SystemUser
 
 class InspectionDetailResponsible(TimeStampModel):
-    inpection_detail_id = models.ForeignKey( InspectionDetail, on_delete = models.PROTECT )
+    inspection_detail_id = models.ForeignKey( InspectionDetail, on_delete = models.PROTECT )
     user_id             = models.ForeignKey( SystemUser, on_delete = models.PROTECT, null = True, blank = True )
     user_fullname       = models.CharField( 'user fullname', max_length = 150 )
     user_dni            = models.CharField( 'user dni', max_length = 15 )
-    user_phone           = models.CharField( 'user phone', max_length = 15, null = True, blank = True )
-    user_email            = models.EmailField( 'user email', max_length = 64, null = True, blank = True )
+    user_phone          = models.CharField( 'user phone', max_length = 15, null = True, blank = True )
+    user_email          = models.EmailField( 'user email', max_length = 64, null = True, blank = True )
 
     class Meta:
         db_table = 'InspectionDetailResponsible'
