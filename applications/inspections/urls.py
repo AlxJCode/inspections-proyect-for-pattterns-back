f47798329be500652c2493a74c8bc032eb9a083b
alex <alexarroyop31@gmail.com>
2023-01-09 12:10:05 -0500

    inspection url

--- applications/inspections/urls.py
+++ applications/inspections/urls.py
@@ -17,38 +17,38 @@ urlpatterns = [
     path('inspections/filters/', InspectionFiltersView.as_view()),
 
     # Inspection Users 
-    path('inpection-users/', InspectionUserListView.as_view()),
-    path('inpection-users/<int:pk>/', InspectionUserDetailView.as_view()),
-    path('inpection-users/filters/', InspectionUserFiltersView.as_view()),
+    path('inspection-users/', InspectionUserListView.as_view()),
+    path('inspection-users/<int:pk>/', InspectionUserDetailView.as_view()),
+    path('inspection-users/filters/', InspectionUserFiltersView.as_view()),
 
     # Inspection Types 
-    path('inpection-types/', InspectionTypeListView.as_view()),
-    path('inpection-types/<int:pk>/', InspectionTypeDetailView.as_view()),
-    path('inpection-types/filters/', InspectionTypeFiltersView.as_view()),
+    path('inspection-types/', InspectionTypeListView.as_view()),
+    path('inspection-types/<int:pk>/', InspectionTypeDetailView.as_view()),
+    path('inspection-types/filters/', InspectionTypeFiltersView.as_view()),
 
     # Inspection Details 
-    path('inpection-details/', InspectionDetailListView.as_view()),
-    path('inpection-details/<int:pk>/', InspectionDetailDetailView.as_view()),
-    path('inpection-details/filters/', InspectionDetailFiltersView.as_view()),
+    path('inspection-details/', InspectionDetailListView.as_view()),
+    path('inspection-details/<int:pk>/', InspectionDetailDetailView.as_view()),
+    path('inspection-details/filters/', InspectionDetailFiltersView.as_view()),
 
     # Inspection Details evidence
-    path('inpection-detail-evidences/', InspectionDetailEvidenceListView.as_view()),
-    path('inpection-detail-evidences/<int:pk>/', InspectionDetailEvidenceDetailView.as_view()),
-    path('inpection-detail-evidences/filters/', InspectionDetailEvidenceFiltersView.as_view()),
+    path('inspection-detail-evidences/', InspectionDetailEvidenceListView.as_view()),
+    path('inspection-detail-evidences/<int:pk>/', InspectionDetailEvidenceDetailView.as_view()),
+    path('inspection-detail-evidences/filters/', InspectionDetailEvidenceFiltersView.as_view()),
 
     # Inspection Details responsible
-    path('inpection-detail-responsibles/', InspectionDetailResponsibleListView.as_view()),
-    path('inpection-detail-responsibles/<int:pk>/', InspectionDetailResponsibleDetailView.as_view()),
-    path('inpection-detail-responsibles/filters/', InspectionDetailResponsibleFiltersView.as_view()),
+    path('inspection-detail-responsibles/', InspectionDetailResponsibleListView.as_view()),
+    path('inspection-detail-responsibles/<int:pk>/', InspectionDetailResponsibleDetailView.as_view()),
+    path('inspection-detail-responsibles/filters/', InspectionDetailResponsibleFiltersView.as_view()),
 
     # Inspection Affected
-    path('inpection-affected/', InspectionAffectedListView.as_view()),
-    path('inpection-affected/<int:pk>/', InspectionAffectedDetailView.as_view()),
-    path('inpection-affected/filters/', InspectionAffectedFiltersView.as_view()),
+    path('inspection-affected/', InspectionAffectedListView.as_view()),
+    path('inspection-affected/<int:pk>/', InspectionAffectedDetailView.as_view()),
+    path('inspection-affected/filters/', InspectionAffectedFiltersView.as_view()),
 
     # Inspection Assessment
-    path('inpection-assessments/', InspectionAssessmentListView.as_view()),
-    path('inpection-assessments/<int:pk>/', InspectionAssessmentDetailView.as_view()),
-    path('inpection-assessments/filters/', InspectionAssessmentFiltersView.as_view()),
+    path('inspection-assessments/', InspectionAssessmentListView.as_view()),
+    path('inspection-assessments/<int:pk>/', InspectionAssessmentDetailView.as_view()),
+    path('inspection-assessments/filters/', InspectionAssessmentFiltersView.as_view()),
 
 ]
