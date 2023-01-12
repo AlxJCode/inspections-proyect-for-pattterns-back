64db276599b484c966fb160a6881b81dab579fa3
alex <alexarroyop31@gmail.com>
2023-01-12 11:02:26 -0500

    id user in notification responsibles

--- applications/inspections/api_views/inspection_detail_respnsible_views.py
+++ applications/inspections/api_views/inspection_detail_respnsible_views.py
@@ -51,10 +51,10 @@ class InspectionDetailResponsibleListView(APIView, PageNumberPagination):
                 inspection_detail_responsible_serializer.save()
                 
                 inspection = InspectionDetailResponsible.objects.filter( id = inspection_detail_responsible_serializer.data['id'] ).values( 'id', 'created' ).last()
-                responsible = SystemUser.objects.filter( dni = inspection_detail_responsible_serializer.data['user_dni'] ).values('auth_user__id').last( )
+                responsible = SystemUser.objects.filter( dni = inspection_detail_responsible_serializer.data['user_dni'] ).values('auth_user').last( )
 
                 devices_to_send = FCMDevice.objects.filter(
-                    user = responsible.auth_user__id
+                    user = responsible.auth_user
                 )
 
                 try:
